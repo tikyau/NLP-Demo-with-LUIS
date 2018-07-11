feff1323c9367b5b538eef48f965a23b380ff4ac
Delon Yau <tikyau@gmail.com>
2018-07-11 09:36:09 +0800

    initial commit

---
+++ receiptLUIS/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System;
+using System.Net.Http;
+using System.Web;
+
+namespace receiptLUIS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            txtOutput.Text = "";
+            Cursor = Cursors.Wait;
+            btnSend.IsEnabled = false;
+            string input = txtInput.Text;
+            txtOutput.Text = await MakeRequest(input);
+            btnSend.IsEnabled = true;
+            Cursor = Cursors.Arrow;
+        }
+
+        static async Task<string> MakeRequest(string input)
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // This app ID is for a public sample app that recognizes requests to turn on and turn off lights
+            var luisAppId = "df67dcdb-c37d-46af-88e1-8b97951ca1c2";
+            var subscriptionKey = "a4d85ab6d72a4d83bad5bac9a5496836 ";
+
+            // The request header contains your subscription key
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // The "q" parameter contains the utterance to send to LUIS
+            queryString["q"] = input;
+            //turn on the left light
+            // These optional request parameters are set to their default values
+            queryString["timezoneOffset"] = "0";
+            queryString["verbose"] = "false";
+            queryString["spellCheck"] = "false";
+            queryString["staging"] = "false";
+
+            var uri = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/" + luisAppId + "?" + queryString;
+            var response = await client.GetAsync(uri);
+
+            var strResponseContent = await response.Content.ReadAsStringAsync();
+            // Display the JSON result from LUIS
+            return strResponseContent.ToString();
+        }
+    }
+}
